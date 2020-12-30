43bbdc7c9515dbb662c52859686378295147aa42
taka1226 <taka2637napoleon815@icloud.com>
2020-12-31 00:50:49 +0900

    you can see news lists

--- App.js
+++ App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-    return (
-        <View style={styles.container}>
-            <Text>hello, react native!</Text>
-            <StatusBar style="auto" />
-        </View>
-    );
-}
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
+import ListItem from './components/ListItem';
+import dummyarticles from './dummies/articles.json';
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
 });
+
+export default function App() {
+    const [articles, setArticle] = useState(dummyarticles);
+    return (
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={articles}
+                renderItem={({ item }) => (
+                    <ListItem
+                        imageUrl={item.urlToImage}
+                        title={item.title}
+                        author={item.author}
+                    />
+                )}
+                keyExtractor={(item, index) => index.toString()}
+            />
+        </SafeAreaView>
+    );
+}
