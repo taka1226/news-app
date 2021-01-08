f59fa83aa5cc08bf5abf9020d15ee0146e6876ce
taka1226 <taka2637napoleon815@icloud.com>
2021-01-08 16:58:50 +0900

    clipの作成

--- screens/ArticleScreen.js
+++ screens/ArticleScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Text } from 'react-native';
-import {WebView} from "react-native-webview";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
 
 const styles = StyleSheet.create({
     container: {
@@ -9,11 +12,26 @@ const styles = StyleSheet.create({
     },
 });
 
-export default function ArticleScreen({route}) {
-    const {article} = route.params;
+export default function ArticleScreen({ route }) {
+    const { article } = route.params;
+    const dispatch = useDispatch();
     return (
         <SafeAreaView style={styles.container}>
-          <WebView source={{uri: article.url}} />
+            <TouchableOpacity
+                onPress={() => {
+                    dispatch(addClip({ clip: article }));
+                }}
+            >
+                <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                onPress={() => {
+                    dispatch(deleteClip({ clip: article }));
+                }}
+            >
+                <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+            </TouchableOpacity>
+            <WebView source={{ uri: article.url }} />
         </SafeAreaView>
     );
 }
